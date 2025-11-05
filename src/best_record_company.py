8d4e4f519e8a389a4b0583386a631c6bd88dae87
26nsmall <26nsmall@gannacademy.org>
2025-11-05 13:20:15 -0500

    wefhuolihvg
    34 uihsDFbnmcSDyuilq43tlushg vluybzthb0v8YO,.DKI,3MC

--- src/best_record_company.py
+++ src/best_record_company.py
@@ -3,7 +3,11 @@
 import pandas as pd
 
 def best_record_company():
-    pass
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    newdf = df.groupby("Publisher")["WoC"].sum()
+    max = newdf.idxmax()
+    mask = df["Publisher"] == max
+    return df[mask]
 
 def main():
     best_record_company()
